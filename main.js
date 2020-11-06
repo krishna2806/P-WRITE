8f94a26116ba5b645e8f4bc8b0f8b9704299eb61
krishna2806 <73472767+krishna2806@users.noreply.github.com>
2020-11-06 20:01:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var INPUTS=[]
+function getp1(){
+for(i=1;i<=6;i++){
+INPUTS.push(document.getElementById("no_"+i).value);
+}
+document.getElementById("p-1").innerHTML=inputs.join;
+}
+function getp2(){
+    for(i=6;i<=10;i++){
+    INPUTS.push(document.getElementById("no_"+i).value);
+    }
+    document.getElementById("p-2").innerHTML=inputs.join(".");
+    }
